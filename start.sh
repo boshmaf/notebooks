67d4b235d4d6d46bc1eae045e2488880a662cde2
Yazan Boshmaf <yazan.boshmaf@gmail.com>
2015-02-16 19:02:06 -0800

    Started with setup

---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ipython nbconvert swi.ipynb --to slides --reveal-prefix reveal.js --post serve
--- start_presentation.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ipython nbconvert ch1_intro.ipynb --to slides --reveal-prefix ../reveal.js --post serve
